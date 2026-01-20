4fbdd08b6667a42c17bac6f116b78dba81a44dc5
R6233M <kukuri.hi6233@gmail.com>
2026-01-20 17:19:30 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,5 +4,8 @@ let bodyWeight = 68;
 // 変数の宣言・値の代入
 let bodyHeight = 1.7;
 
+// 変数の宣言・値の代入
+let bodyMassIndex = bodyWeight / (bodyHeight * bodyHeight);
+
 // コンソールへの出力
 console.log(bodyWeight / (bodyHeight * bodyHeight));
