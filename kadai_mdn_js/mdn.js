8f951470b04a9c3996fb08c2c7f5f1892f5a0d5a
R6233M <kukuri.hi6233@gmail.com>
2026-01-24 15:53:52 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,5 @@
-// 参考にしたURL：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
-
-const event = new Date(Date.UTC(2026, 0, 24));
+// 参考URL：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString#%E6%A7%8B%E6%96%87
+const event = new Date();
 const options = {
   year: "numeric",
   month: "long",
