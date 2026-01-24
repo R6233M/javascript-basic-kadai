6ccd5fb8f4011a52d8bf2a941527a9c3bfd03587
R6233M <kukuri.hi6233@gmail.com>
2026-01-24 17:13:52 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -3,7 +3,23 @@ const event = new Date();
 const options = {
   year: "numeric",
   month: "long",
-  day: "numeric",
-};
+  day: "numeric"
+}
+console.log(event.toLocaleDateString('ja-JP', options));
 
-console.log(event.toLocaleDateString(undefined, options));
+/*
+// getFullYearについて：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
+const today = new Date();
+const fullYear = today.getFullYear();
+console.log(fullYear);
+
+// getMonthについて：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/getMonth
+const today1 = new Date();
+const month =today1.getMonth();
+console.log(month);
+
+// getDateについて：https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/getDate
+const today2 = new Date();
+const day = today2.getDate();
+console.log(day);
+*/
